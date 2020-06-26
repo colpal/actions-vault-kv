392d447309bbddfec6ff742153192beeb627e6aa
Rudra Desai <rudra_desai@colpal.com>
2020-06-26 10:47:24 -0400

    Changed name of variable

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    const path = core.getInput('path');
+    const path = core.getInput('vaultPath');
     const creds = {username: "test", password: "test123"}
 
     console.log(`Path is: ${path}`)
     core.setOutput("creds", creds);
-    
+
   } catch (error) {
     core.setFailed(error.message);
 }
