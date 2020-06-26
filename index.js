e19efaf440987c000e30832f0500704ac3816574
Rudra Desai <rudra_desai@colpal.com>
2020-06-26 10:22:28 -0400

    Test Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    const path = core.getInput('path');
+    const creds = {username: "test", password: "test123"}
+
+    console.log(`Path is: ${path}`)
+    core.setOutput("creds", creds);
+    
+  } catch (error) {
+    core.setFailed(error.message);
+}
