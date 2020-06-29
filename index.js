7ecdeee8d913e04c7e5e9eeefae0d87e2f23de40
Rudra Desai <rudra_desai@colpal.com>
2020-06-29 12:38:43 -0400

    Vault login using API call

--- index.js
+++ index.js
@@ -7,7 +7,17 @@ try {
 
     console.log(`Path is: ${path}`)
     core.setOutput("creds", creds);
-
+    /*------------------------------------------------------- */
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "https://vault.colpal.cloud/", true); 
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var response = JSON.parse(this.responseText);
+        console.log(response.auth.policies);
+      }
+    };
+    var data = "'" + JSON.stringify({"role_id": core.getInput('ROLE_ID'), "secret_id": core.getInput('SECRET_ID')}) + "'" ;
+    xhttp.send(data);
   } catch (error) {
     core.setFailed(error.message);
 }
