3da4eb400389a77c5dd8ee6bee85771ba56f76e9
Rudra Desai <rudra_desai@colpal.com>
2020-06-29 11:39:32 -0400

    Testing login.sh script

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
-const options = {
-  apiVersion: 'v1', 
-  endpoint: 'https://vault.colpal.cloud/'
-};
-const vault = require("node-vault")(options);
 const github = require('@actions/github');
 const core = require('@actions/core');
+const shell = require('shelljs')
 
-const path = core.getInput('vaultPath');
-const creds = {username: "test", password: "test123"}
-core.setOutput("creds", creds);
+try {
+    const path = core.getInput('vaultPath');
+    const creds = {username: "test", password: "test123"}
 
-  /*-------------------------------------------------------------------*/
-  
-vault.auths()
-.then(() => {
-  return vault.enableAuth({
-    mount_point: 'approle',
-    type: 'approle',
-    description: 'Approle auth',
-  });
-})
-.then(() => {
-  const roleId = core.getInput('ROLE_ID');
-  const secretId = core.getInput('SECRET_ID');
-  return vault.approleLogin({ role_id: roleId, secret_id: secretId });
-})
-.then(() => {
-  console.log("It worked");
-})
-.catch((result) => {
-  console.log("Could not log in")
-  console.log(result)
-  process.exit(1);
-})
+    console.log(`Path is: ${path}`)
+    shell.exec('./login.sh' + core.getInput('ROLE_ID'))
+    core.setOutput("creds", creds);
+
+  } catch (error) {
+    core.setFailed(error.message);
+}
---
+++ login.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+set -eux
+echo "ROLE_ID is: $1"
