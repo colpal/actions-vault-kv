43a678916a6c705e8717d475b890ce38febb7fdf
Rudra Desai <rudra_desai@colpal.com>
2020-06-29 10:47:33 -0400

    Test Commit for ROLE ID

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const core = require('@actions/core');
+const options = {
+  apiVersion: 'v1', 
+  endpoint: 'https://vault.colpal.cloud/'
+};
+const vault = require("node-vault")(options);
 const github = require('@actions/github');
+const core = require('@actions/core');
 
 try {
     const path = core.getInput('vaultPath');
     const creds = {username: "test", password: "test123"}
 
     console.log(`Path is: ${path}`)
+    console.log(`ROLE ID: ${core.getInput('ROLE_ID')}`)
     core.setOutput("creds", creds);
 
+  /*-------------------------------------------------------------------*/
+  
   } catch (error) {
     core.setFailed(error.message);
 }
