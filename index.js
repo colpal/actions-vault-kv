348b3bae531f4330e299244d7a3c3e8347e2b3ee
Rudra Desai <rudra_desai@colpal.com>
2020-06-29 12:44:04 -0400

    Installed XMLHttpRequest

--- index.js
+++ index.js
@@ -8,15 +8,16 @@ try {
     console.log(`Path is: ${path}`)
     core.setOutput("creds", creds);
     /*------------------------------------------------------- */
-    var xhttp = new XMLHttpRequest();
+    let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    let xhttp = new XMLHttpRequest();
     xhttp.open("POST", "https://vault.colpal.cloud/", true); 
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        var response = JSON.parse(this.responseText);
+        let response = JSON.parse(this.responseText);
         console.log(response.auth.policies);
       }
     };
-    var data = "'" + JSON.stringify({"role_id": core.getInput('ROLE_ID'), "secret_id": core.getInput('SECRET_ID')}) + "'" ;
+    let data = "'" + JSON.stringify({"role_id": core.getInput('ROLE_ID'), "secret_id": core.getInput('SECRET_ID')}) + "'" ;
     xhttp.send(data);
   } catch (error) {
     core.setFailed(error.message);
