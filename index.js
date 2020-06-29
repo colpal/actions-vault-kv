592653d53d758937ca4a0a0d96e30c718744f642
Rudra Desai <rudra_desai@colpal.com>
2020-06-29 12:05:49 -0400

    Removed login.sh

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const github = require('@actions/github');
 const core = require('@actions/core');
-const shell = require('shelljs')
 
 try {
     const path = core.getInput('vaultPath');
     const creds = {username: "test", password: "test123"}
 
     console.log(`Path is: ${path}`)
-    shell.exec('./login.sh ' + core.getInput('ROLE_ID'))
+    shell.exec('./login.sh ' + core.getInput('ROLE_ID') + ' ' + core.getInput('SECRET_ID'))
     core.setOutput("creds", creds);
 
   } catch (error) {
--- login.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-set -eux
-echo "ROLE_ID is: $1"
