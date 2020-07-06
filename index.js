4ff59db8efdd027ae1e3407e91dda9ea0956c55a
Rudra Desai <rudra_desai@colpal.com>
2020-07-06 15:41:02 -0400

    Testing with paths json

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const github = require('@actions/github');
 const core = require('@actions/core');
 const https = require('https');
-const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
 let token;
 
 try {
     let vaultPath = [];
     let paths = {};
+    let currentPath = "";
     core.getInput('vaultPath').split(",").forEach((param, i) => {
         vaultPath.push(param.trim());
         if (param.includes("/")){
-          paths[i] = param;
+          currentPath = param;
+          paths[currentPath] = [];
         }
+        else 
+          paths[currentPath] = paths[currentPath].push(param.trim());
     });
     console.log(paths);
 /*-------------------Get token and secret----------------------------------- */
@@ -38,45 +41,54 @@ try {
         token = JSON.parse(d).auth.client_token;
         console.log("Login successful!");
 
-        const secretOptions = {
-          hostname: 'vault.colpal.cloud',
-          port: 443,
-          path: '/v1' + vaultPath[0],
-          method: 'GET',
-          headers: {
-            'X-Vault-Token' : token,
-            'Content-Type': 'application/json'
+        for (let i = 0; i < vaultPath; i++)
+        {
+          let secretOptions = {};
+          if(vaultPath[i].includes("/"))
+          {
+            secretOptions = {
+              hostname: 'vault.colpal.cloud',
+              port: 443,
+              path: '/v1' + vaultPath[i],
+              method: 'GET',
+              headers: {
+                'X-Vault-Token' : token,
+                'Content-Type': 'application/json'
+              }
+            }
+
+            let req2 = https.request(secretOptions, (res) => {
+            console.log(`\nstatusCode: ${res.statusCode}`)
+            
+            res.on('data', (d) => {
+              let secret = JSON.parse(d);
+              secret.errors && (console.log(secret) || process.exit(1));
+              console.log("Secret opened!");
+          
+              if(vaultPath.length == 1)
+                core.setOutput("creds", secret.data.data);
+              else if (vaultPath.length == 2)
+                core.setOutput("creds",secret.data.data[vaultPath[1]])
+              else {
+                let returnCreds = {};
+                for (let k = 1; k < vaultPath.length; k++){
+                  returnCreds[vaultPath[k]] = secret.data.data[vaultPath[k]];
+                }
+                core.setOutput("creds",returnCreds)
+              }
+              })
+            })
+            
+            req2.on('error', (error) => {
+              console.error(error)
+              req2.end()
+              process.exit(1);
+            })
+            req2.write(data)
+            req2.end()
           }
         }
-        const req2 = https.request(secretOptions, (res) => {
-        console.log(`\nstatusCode: ${res.statusCode}`)
-        
-        res.on('data', (d) => {
-          const secret = JSON.parse(d);
-          secret.errors && (console.log(secret) || process.exit(1));
-          console.log("Secret opened!");
-      
-          if(vaultPath.length == 1)
-            core.setOutput("creds", secret.data.data);
-          else if (vaultPath.length == 2)
-            core.setOutput("creds",secret.data.data[vaultPath[1]])
-          else {
-            let returnCreds = {};
-            for (let i = 1; i < vaultPath.length; i++){
-              returnCreds[vaultPath[i]] = secret.data.data[vaultPath[i]];
-            }
-            core.setOutput("creds",returnCreds)
-          }
-          })
-        })
-        
-        req2.on('error', (error) => {
-          console.error(error)
-          req2.end()
-          process.exit(1);
-        })
-        req2.write(data)
-        req2.end()
+
       })
     })
     req.on('error', (error) => {
