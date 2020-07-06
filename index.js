46a91a70f14ef35cbae15387d6198c51dbcb990b
Rudra Desai <rudra_desai@colpal.com>
2020-07-06 16:28:52 -0400

    Debugging returned json empty issue

--- index.js
+++ index.js
@@ -67,7 +67,12 @@ try {
               {
                 let returnCreds = {};
                 for (let k = 0; k < paths[onePath].length; k++)
+                {
                   returnCreds[paths[onePath][k]] = secret.data.data[paths[onePath[k]]];
+                  console.log(secret.data.data[paths[onePath[k]]]);
+                }
+                console.log(returnCreds);
+                console.log(paths[onePath].length);
                 core.setOutput("creds", returnCreds);
               }
             })
