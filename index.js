31daaf94f7f451d7b515ec3a3937f35cd8b71560
Rudra Desai <rudra_desai@colpal.com>
2020-07-06 11:08:14 -0400

    Testing to get all credentials from a secret

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ try {
           const secret = JSON.parse(d);
           secret.errors && (console.log(secret) || process.exit(1));
           console.log("Secret opened!");
-          vaultPath.length == 2 && core.setOutput("creds",secret.data.data[vaultPath[1]])
+          vaultPath.length == 2 ? core.setOutput("creds",secret.data.data[vaultPath[1]]) : core.setOutput("creds", secret.data.data)
           })
         })
         
