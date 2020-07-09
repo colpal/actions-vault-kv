1d0c033480198f4b81f13c030519788673954d32
Rudra Desai <rudra_desai@colpal.com>
2020-07-09 12:08:01 -0400

    On push run on this branch

--- index.js
+++ index.js
@@ -1,105 +1,61 @@
 const github = require('@actions/github');
 const core = require('@actions/core');
 const https = require('https');
+let returnCreds = {};
+let paths = {};
 let token;
 
 try {
-  let paths = {};
-  let returnCreds = {};
-  const userInput = (JSON.parse(core.getInput('vaultPath')));
-  for (key in userInput)
-  {
-    if (!paths.hasOwnProperty(userInput[key][0]))
-    	paths[userInput[key][0]] = [];
-    userInput[key].length == 2 ? paths[userInput[key][0]].push(userInput[key].length-1 + ':' + userInput[key][1]) : paths[userInput[key][0]].push(userInput[key].length-1 + ":")        
-    paths[userInput[key][0]].push(key);
-  }
-  console.log(paths);
-  /*-------------------Get token and secret----------------------------------- */
-
-  const data = JSON.stringify({
-    role_id: core.getInput('ROLE_ID'),
-    secret_id: core.getInput('SECRET_ID')
-  })
-  const tokenOptions = {
-    hostname: 'vault.colpal.cloud',
-    port: 443,
-    path: '/v1/auth/approle/login',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': data.length
-    }
-  }
-
-  const req = https.request(tokenOptions, (res) => {
-    console.log(`statusCode: ${res.statusCode}`)
-
-    res.on('data', (d) => {
-      token = JSON.parse(d).auth.client_token;
-      console.log("Login successful!");
-
-      let secretOptions = {};
-          secretOptions = {
-          hostname: 'vault.colpal.cloud',
-          port: 443,
-          path: "",
-          method: 'GET',
-          headers: {
-            'X-Vault-Token': token,
-            'Content-Type': 'application/json'
-          }
-        }
-        for (onePath in paths) {
-        let req2 = https.request(secretOptions, (res) => {
-          req2.path = "/v1" + onePath;
-          console.log(`\nstatusCode: ${res.statusCode}`)
-          res.on('data', (d) => {
-            let secret = JSON.parse(d);
-            secret.errors && (console.log(secret) || process.exit(1));
-            console.log("Secret opened!");
-            console.log("onePath: " + onePath);
-            for (let k = 0; k < paths[onePath].length; k+=2)
-            {
-                let str = paths[onePath][k];
-                let idx = paths[onePath][k].indexOf(":");
-                let thisSecret = "";
-                console.log("str: " + str);
-                console.log("idx: " + idx);
-                console.log("str[idx-1]: " + str[idx-1]);
-                console.log("str[idx-1] == 1: " + (str[idx-1] == 1));
-                if (str[idx-1] == 1)
-                {
-                  thisSecret = str.substr(idx+1)
-                  console.log(thisSecret);
-                  returnCreds[paths[onePath][k+1]] = secret.data.data[thisSecret];
-                }
-                else
-                    returnCreds[paths[onePath][k+1]] = secret.data.data;  
-                console.log("k: " + k);
-                console.log("returnCreds[paths[onePath][k+1]]: " + returnCreds[paths[onePath][k+1]]);
-                console.log("secret.data.data[thisSecret]: " + secret.data.data[thisSecret]);
-            }
-            console.log(returnCreds);
-          })
-        })
-        req2.on('error', (error) => {
-          console.error(error)
-          req2.end()
-          process.exit(1);
-        })
-        req2.write(data)
-        req2.end()
-      }
+    const data = JSON.stringify({
+        role_id: core.getInput('ROLE_ID'),
+        secret_id: core.getInput('SECRET_ID')
     })
-  })
-  req.on('error', (error) => {
-    console.error(error)
-    req.end()
-    process.exit(1);
-  })
-  req.write(data)
-  req.end()
+    const tokenOptions = {
+        hostname: 'vault.colpal.cloud',
+        port: 443,
+        path: '/v1/auth/approle/login',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': data.length
+        }
+      }
+
+    (async function () {
+        try {
+            let loginPromise = getToken(tokenOptions, data);
+            let loginResponse = await loginPromise;
+            console.log(loginResponse); //Promise response
+        } catch(e) {
+            console.log(e);
+        }
+    })();
+
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
+}
+
+function getToken(options, data) {
+    return new Promise((resolve, reject) => {
+        const req = https.request(options, (res) => {
+            console.log(`statusCode: ${res.statusCode}`)
+        
+            res.on('data', (d) => {
+              token = JSON.parse(d).auth.client_token;
+              console.log("Login successful!");
+            })
+        
+            res.on('end', () => {
+                let response_body = Buffer.concat(chunks_of_data);
+                resolve(response_body.toString());
+            })
+
+            req.on('error', (error) => {
+                console.error(error)
+                reject(error);
+            })
+        req.write(data)
+        req.end()
+        })
+    })
 }
