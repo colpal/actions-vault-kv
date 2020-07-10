c67b5c38230acec5e10bc0784dcd4e5c4a02b328
Rudra Desai <rudra_desai@colpal.com>
2020-07-10 08:17:37 -0400

    Gave aysnc function a name

--- index.js
+++ index.js
@@ -5,35 +5,31 @@ let returnCreds = {};
 let paths = {};
 let token;
 
-try {
-    const data = JSON.stringify({
-        role_id: core.getInput('ROLE_ID'),
-        secret_id: core.getInput('SECRET_ID')
-    })
-    const tokenOptions = {
-        hostname: 'vault.colpal.cloud',
-        port: 443,
-        path: '/v1/auth/approle/login',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Content-Length': data.length
-        }
-      }
-
-    (async function () {
-        try {
-            let loginPromise = getToken(tokenOptions, data);
-            let loginResponse = await loginPromise;
-            console.log(loginResponse); //Promise response
-        } catch(e) {
-            console.log(e);
-        }
-    })();
-
-} catch (error) {
-    core.setFailed(error.message);
+const data = JSON.stringify({
+    role_id: core.getInput('ROLE_ID'),
+    secret_id: core.getInput('SECRET_ID')
+})
+const tokenOptions = {
+    hostname: 'vault.colpal.cloud',
+    port: 443,
+    path: '/v1/auth/approle/login',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': data.length
+    }
 }
+
+async function fetch (request) {
+    try {
+        let loginPromise = getToken(tokenOptions, data);
+        let loginResponse = await loginPromise;
+        console.log(loginResponse); //Promise response
+    } catch(e) {
+        console.log(e);
+    }
+}
+fetch();
 
 function getToken(options, data) {
     return new Promise((resolve, reject) => {
