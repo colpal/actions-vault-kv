28039614edd010a73abf506b6cf27a3abc8c7663
Rudra Desai <rudra_desai@colpal.com>
2020-07-10 08:22:17 -0400

    Added a try/catch

--- index.js
+++ index.js
@@ -5,53 +5,56 @@ let returnCreds = {};
 let paths = {};
 let token;
 
-const data = JSON.stringify({
-    role_id: core.getInput('ROLE_ID'),
-    secret_id: core.getInput('SECRET_ID')
-})
-const tokenOptions = {
-    hostname: 'vault.colpal.cloud',
-    port: 443,
-    path: '/v1/auth/approle/login',
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': data.length
-    }
-}
-
-async function fetch (request) {
-    try {
-        let loginPromise = getToken(tokenOptions, data);
-        let loginResponse = await loginPromise;
-        console.log(loginResponse); //Promise response
-    } catch(e) {
-        console.log(e);
-    }
-}
-fetch();
-
-function getToken(options, data) {
-    return new Promise((resolve, reject) => {
-        const req = https.request(options, (res) => {
-            console.log(`statusCode: ${res.statusCode}`)
-        
-            res.on('data', (d) => {
-              token = JSON.parse(d).auth.client_token;
-              console.log("Login successful!");
-            })
-        
-            res.on('end', () => {
-                let response_body = Buffer.concat(chunks_of_data);
-                resolve(response_body.toString());
-            })
-
-            req.on('error', (error) => {
-                console.error(error)
-                reject(error);
-            })
-        req.write(data)
-        req.end()
-        })
+try {
+    const data = JSON.stringify({
+        role_id: core.getInput('ROLE_ID'),
+        secret_id: core.getInput('SECRET_ID')
     })
+    const tokenOptions = {
+        hostname: 'vault.colpal.cloud',
+        port: 443,
+        path: '/v1/auth/approle/login',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': data.length
+        }
+    }
+
+    async function fetch (request) {
+        try {
+            let loginPromise = getToken(tokenOptions, data);
+            let loginResponse = await loginPromise;
+            console.log(loginResponse); //Promise response
+        } catch(e) {
+            console.log(e);
+        }
+    }
+    fetch();
+
+    function getToken(options, data) {
+        return new Promise((resolve, reject) => {
+            const req = https.request(options, (res) => {
+                console.log(`statusCode: ${res.statusCode}`)
+            
+                res.on('data', (d) => {
+                token = JSON.parse(d).auth.client_token;
+                console.log("Login successful!");
+                })
+            
+                res.on('end', () => {
+                    let response_body = Buffer.concat(chunks_of_data);
+                    resolve(response_body.toString());
+                })
+
+                req.on('error', (error) => {
+                    console.error(error)
+                    reject(error);
+                })
+            req.write(data)
+            })
+        })
+    }
+} catch (error) {
+    core.setFailed(error.message);
 }
