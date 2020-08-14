2a93a8ea62bda86201ad8773780c8cad714ade70
Nicholas Farley <nicholasafarley@gmail.com>
2020-08-14 13:12:10 -0400

    Replace console method with core logger

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const { GoogleAuth } = require('google-auth-library');
 
 function fail(error, message) {
-  console.error(JSON.stringify(error, null, 2));
+  core.error(error);
   core.setFailed(message);
   process.exit(1);
 }
