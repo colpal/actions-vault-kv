15efbc93b7405ab0e704dfe4deedcaaf148dc1a2
Nicholas Farley <nicholasafarley@gmail.com>
2020-08-14 11:54:03 -0400

    Refactor to use Google Auth client library

--- src/index.js
+++ src/index.js
@@ -1,56 +1,9 @@
 const core = require('@actions/core');
-const https = require('https');
-
-const loginBody = JSON.stringify({
-  role_id: core.getInput('role-id', { required: true }),
-  secret_id: core.getInput('secret-id', { required: true }),
-});
-const tokenOptions = {
-  hostname: 'vault.colpal.cloud',
-  port: 443,
-  path: '/v1/auth/approle/login',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Content-Length': loginBody.length,
-  },
-};
-const secretOptions = {
-  hostname: 'vault.colpal.cloud',
-  port: 443,
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-};
+const { GoogleAuth } = require('google-auth-library');
 
 function fail(message) {
   core.setFailed(message);
   process.exit(1);
-}
-
-function fetch(options, data) {
-  return new Promise((resolve, reject) => {
-    const req = https.request(options, (res) => {
-      const body = [];
-      res.on('data', (d) => {
-        body.push(d);
-      });
-      res.on('end', () => {
-        const response = JSON.parse(body.join(''));
-        if (response.errors) {
-          reject({ status: res.statusCode, err: response });
-        }
-        resolve({ status: res.statusCode, val: response });
-      });
-    });
-    req.on('error', (error) => {
-      console.error(error);
-      reject({ status: res.statusCode, err: error });
-    });
-    req.write(data);
-    req.end();
-  });
 }
 
 function setValues(paths, userInput) {
@@ -66,11 +19,47 @@ function setValues(paths, userInput) {
 }
 
 async function main() {
+  const roleID = core.getInput('role-id', { required: true });
+  const secretID = core.getInput('secret-id', { required: true });
+  const secretPaths = core.getInput('secret-paths', { required: true });
+  const serviceAccountKey = core.getInput('service-account-key', { required: true });
+  const vaultAddress = core.getInput('vault-address');
+  const clientID = core.getInput('iap-client-id');
+
   let userInput;
   try {
-    userInput = JSON.parse(core.getInput('secret-paths', { required: true }));
+    userInput = JSON.parse(secretPaths);
   } catch (error) {
     fail(`Could not parse your input for 'secret-paths'. Make sure 'secret-paths' is a valid JSON object\n${error}`);
+  }
+
+  let credentials;
+  try {
+    credentials = JSON.parse(Buffer.from(serviceAccountKey, 'base64').toString('utf8'));
+  } catch (e) {
+    fail(`Could not parse 'service-account-key' as JSON: ${e}`);
+  }
+
+  let client;
+  try {
+    client = await (new GoogleAuth({ credentials })).getIdTokenClient(clientID);
+  } catch (e) {
+    fail(`Could not create a valid Google Auth client: ${e}`);
+  }
+
+  let vaultToken;
+  try {
+    const tokenReponse = await client.request({
+      url: `${vaultAddress}/v1/auth/approle/login`,
+      method: 'post',
+      data: {
+        role_id: roleID,
+        secret_id: secretID,
+      },
+    });
+    vaultToken = tokenReponse.val.auth.client_token;
+  } catch (res) {
+    fail(`Could not log you in, check your Role ID and Secret ID!\n${res.err.errors}`);
   }
 
   const paths = {};
@@ -78,25 +67,25 @@ async function main() {
     paths[path] = null;
   });
 
-  try {
-    const tokenReponse = await fetch(tokenOptions, loginBody);
-    secretOptions.headers['X-Vault-Token'] = tokenReponse.val.auth.client_token;
-  } catch (res) {
-    fail(`Could not log you in, check your Role ID and Secret ID!\n${res.err.errors}`);
-  }
-
   const regex = /\/?secret\/(.*)/;
-  const responses = await Promise.all(Object.keys(paths).map(async (onePath) => {
-    const [, capture] = onePath.match(regex);
-    const path = `/v1/secret/data/${capture}`;
-    try {
-      const response = await fetch({ ...secretOptions, path }, loginBody);
-      response.ACTUAL_PATH = onePath;
-      return response;
-    } catch (res) {
-      fail(`Could not open: ${onePath}. Check that the path is valid.\n${JSON.stringify(res)}`);
-    }
-  }));
+  const promises = Object
+    .keys(paths)
+    .map(async (onePath) => {
+      const [, capture] = onePath.match(regex);
+      try {
+        const response = await client.request({
+          url: `${vaultAddress}/v1/secret/data/${capture}`,
+          method: 'get',
+          headers: {
+            'X-Vault-Token': vaultToken,
+          },
+        });
+        return { ...response, ACTUAL_PATH: onePath };
+      } catch (res) {
+        return fail(`Could not open: ${onePath}. Check that the path is valid.\n${JSON.stringify(res)}`);
+      }
+    });
+  const responses = await Promise.all(promises);
   responses.forEach((response) => {
     paths[response.ACTUAL_PATH] = response.val.data.data;
   });
